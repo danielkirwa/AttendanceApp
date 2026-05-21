da099a1e45f37685eb9b39f383a92855ef7d6206
danielkirwa <kirwadan03@gmail.com>
2026-05-21 14:46:13 +0300

    All active course details picked ready for gps

--- app/src/main/java/com/example/attendanceapp/StudentDashboard.java
+++ app/src/main/java/com/example/attendanceapp/StudentDashboard.java
@@ -24,7 +24,7 @@ import com.google.firebase.database.ValueEventListener;
 public class StudentDashboard extends AppCompatActivity {
 
     Button btnLogout, btnMyCourses,btnMarkAttendance;
-    TextView txtdeviceid,txtStatus,txtrealdeviceID;
+    TextView txtdeviceid,txtStatus,txtrealdeviceID,txtCourseName,txtVenue,txtTimer,txtsessionTitle,txtGps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +37,11 @@ public class StudentDashboard extends AppCompatActivity {
         txtStatus = findViewById(R.id.txtStatus);
         txtrealdeviceID = findViewById(R.id.txtrealdeviceID);
         btnMarkAttendance = findViewById(R.id.btnMarkAttendance);
+        txtCourseName = findViewById(R.id.txtCourseName);
+        txtVenue = findViewById(R.id.txtVenue);
+        txtTimer = findViewById(R.id.txtTimer);
+        txtsessionTitle = findViewById(R.id.txtsessionTitle);
+        txtGps = findViewById(R.id.txtGps);
 
 
         // get device id
@@ -91,7 +96,56 @@ public class StudentDashboard extends AppCompatActivity {
                                 Toast.LENGTH_SHORT).show();
                     }
                 });
+ // ==================== check which course student is enrolled in +=====
 
+        String studentEmail = user.getEmail();
+
+        String formattedEmail = studentEmail.replace(".", "_")
+                .replace("@", "_at_");
+
+        DatabaseReference refEnrollments = FirebaseDatabase
+                .getInstance("https://attendanceapp-bb425-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference("Enrollments");
+
+        refEnrollments.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean found = false;
+                for (DataSnapshot courseSnapshot : snapshot.getChildren()) {
+                    // check if this user exists inside current course
+                    if (courseSnapshot.child(formattedEmail).exists()) {
+                        found = true;
+                        String courseCode = courseSnapshot
+                                .child(formattedEmail)
+                                .child("courseCode")
+                                .getValue(String.class);
+                        String status = courseSnapshot
+                                .child(formattedEmail)
+                                .child("status")
+                                .getValue(String.class);
+                        Toast.makeText(StudentDashboard.this,
+                                "Enrolled in: " + courseCode,
+                                Toast.LENGTH_LONG).show();
+                        loadCourseDetails(courseCode);
+                        break;
+                    }
+                }
+                if (!found) {
+                    Toast.makeText(StudentDashboard.this,
+                            "Student not enrolled",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+                Toast.makeText(StudentDashboard.this,
+                        error.getMessage(),
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
         // ================= MY COURSES =================
@@ -114,5 +168,110 @@ public class StudentDashboard extends AppCompatActivity {
 
             finish();
         });
+    }
+
+    private void loadCourseDetails(String courseCode) {
+
+        DatabaseReference refCourses = FirebaseDatabase
+                .getInstance("https://attendanceapp-bb425-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference("CourseSessions");
+
+        refCourses.child(courseCode)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        if (!snapshot.exists()) {
+                            Toast.makeText(StudentDashboard.this,
+                                    "Course not found",
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        boolean foundOpenSession = false;
+
+                        // LOOP sessions
+                        for (DataSnapshot sessionSnap : snapshot.getChildren()) {
+
+                            String status = sessionSnap.child("status").getValue(String.class);
+
+                            if (status != null && status.equals("open")) {
+
+                                foundOpenSession = true;
+
+                                String date = sessionSnap.child("date").getValue(String.class);
+                                String time = sessionSnap.child("time").getValue(String.class);
+                                String venueCode = sessionSnap.child("venueCode").getValue(String.class);
+                                String lecturerEmail = sessionSnap.child("lecturerEmail").getValue(String.class);
+                                Long duration = sessionSnap.child("duration").getValue(Long.class);
+
+                                String courseCode = sessionSnap.child("courseCode").getValue(String.class);
+
+                                txtCourseName.setText(courseCode);
+                                txtVenue.setText(venueCode);
+                                txtTimer.setText("Date : " + date + " Time : " + time);
+                                txtsessionTitle.setText(lecturerEmail);
+                                loadVenueCoordinates(venueCode);
+                                break; // stop after first open session
+                            }
+                        }
+
+                        if (!foundOpenSession) {
+                            Toast.makeText(StudentDashboard.this,
+                                    "No active sessions (closed or upcoming)",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                        Toast.makeText(StudentDashboard.this,
+                                error.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void loadVenueCoordinates(String venueCode) {
+
+        DatabaseReference refVenues = FirebaseDatabase
+                .getInstance("https://attendanceapp-bb425-default-rtdb.europe-west1.firebasedatabase.app/")
+                .getReference("GPSVenues");
+
+        refVenues.child(venueCode)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                        if (!snapshot.exists()) {
+                            Toast.makeText(StudentDashboard.this,
+                                    "Venue not found",
+                                    Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        Double latitude = snapshot.child("latitude").getValue(Double.class);
+                        Double longitude = snapshot.child("longitude").getValue(Double.class);
+                        String radius = snapshot.child("radius").getValue(String.class);
+                        String venueName = snapshot.child("venueName").getValue(String.class);
+
+                        txtGps.setText(
+                                venueName + "\nLat: " + latitude +
+                                        "\nLng: " + longitude +
+                                        "\nRadius: " + radius + "m"
+                        );
+
+                    }
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                        Toast.makeText(StudentDashboard.this,
+                                error.getMessage(),
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
